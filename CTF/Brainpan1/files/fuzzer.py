3e45ce5b4b30c2be2c109b16525722e4c985824a
KakakSeram <161952859+KakakSeram@users.noreply.github.com>
2024-08-01 14:16:47 +0700

    Adding files

--- CTF/Brainpan1/files/fuzzer.py
+++ CTF/Brainpan1/files/fuzzer.py
@@ -1,12 +1,26 @@
-#!/usr/share/python
+#!/usr/bin/python3
 
 import socket, time, sys
 
-ip = "10.37.1.149"
-port = 9999
+ip = "10.37.1.149"	# Change to IP Host
+port = 9999		# Change to Port Host
 timeout = 5
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((ip, port))
-s.send(b"AAAA \r\n")
+string = "A" * 100
 
+print("##### Starting Fuzzing #####")
+while True:
+  try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(timeout)
+      s.connect((ip, port))
+      s.recv(1024)
+      print("Fuzzing with {} bytes".format(len(string)))
+      s.send(bytes(string + "\r\n", "latin-1"))
+      s.recv(1024)
+  except:
+    print("##### End of Fuzzing #####")
+    print("Fuzzing crashed at {} bytes".format(len(string)))
+    sys.exit(0)
+  string += 100 * "A"
+  time.sleep(1)
